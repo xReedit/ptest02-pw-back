3779dab88a254b02255bf5467a2059b672a89612
marcelo <xreedit@gmail.com>
2019-09-14 15:56:00 -0500

    cors 1.4

--- app.js
+++ app.js
@@ -40,11 +40,13 @@ app.use(function(err, req, res, next) {
 
 // sockets
 var server = http.createServer(app);
-var io = socketIo(server);
+
 
 server.listen(config.port, function () {
     console.log('Server is running.. port '+ config.port); 
 });
+
+var io = socketIo(server);
 
 // var socketIo = require('socket.io')(server, {
 //   path: '/api-pwa/socket.io'

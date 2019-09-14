5d0579b5bf56f26e1dd064b843d276d10d09828c
marcelo <xreedit@gmail.com>
2019-09-14 02:33:33 -0500

    cors 1.1

--- app.js
+++ app.js
@@ -1,25 +1,14 @@
-var express = require("express")(); 
+var express = require("express"); 
 var bodyParser = require('body-parser');
 var cors=require('cors');
 var app = express();
 var config = require('./config');
 var socketsController = require('./controllers/sockets');
 
-// var socket = require('socket.io');
-// var http = require('http');
-// var server = http.createServer(app);
-
 app.use(cors());
 app.use(bodyParser.json()); // soporte para bodies codificados en jsonsupport
 app.use(bodyParser.urlencoded({ extended: true })); // soporte para bodies codificados
 
-// CORS
-// app.use(function(req, res, next) {
-//     res.header("Access-Control-Allow-Origin", "*");
-//     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, *");
-//     res.header("Access-Control-Allow-Methods", "POST, GET, PUT, DELETE, OPTIONS");
-//     next();
-// });
 
 // para pwa-app-pedidos
 var appV3 = require('./routes/v3');
@@ -46,12 +35,8 @@ app.use(function(err, req, res, next) {
 
 
 // sockets
-
-// const server = require('http').createServer(app);
-var http = require('http');
-var server = http.createServer(app);
+const server = require('http').Server(app);
 const io = require('socket.io')(server);
-// var io = socket.listen(server);
 
 socketsController.socketsOn(io);
 

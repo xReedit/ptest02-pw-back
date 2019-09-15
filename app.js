7090cf9c55052ad6a1995ebd3401f2d01b034b24
marcelo <xreedit@gmail.com>
2019-09-15 12:26:14 -0500

    cors 1.5

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-// const http = require('http');
+const http = require('http');
 const express = require("express"); 
 const socketIo = require('socket.io')
 
@@ -40,27 +40,27 @@ app.use(function(err, req, res, next) {
 
 // sockets
 
-// var server = http.createServer(app);
-// server.listen(config.port, function () {
-//     console.log('Server is running.. port '+ config.port); 
-// });
+var server = http.createServer(app);
+server.listen(config.port, function () {
+    console.log('Server is running.. port '+ config.port); 
+});
 
 // // desarrollo
 // var io = socketIo(server);
 
 // produccion
-// var io = socketIo(server, {
-//   path: '/api-pwa/socket.io'
-// });
-
-const server = require('http').createServer(app);
-const io = require('socket.io')(server, {
+var io = socketIo(server, {
   path: '/api-pwa/socket.io'
 });
 
-server.listen(config.port, function () {
-    console.log('Server is running.. port '+ config.port); 
-});
+// const server = require('http').createServer(app);
+// const io = require('socket.io')(server, {
+//   path: '/api-pwa/socket.io'
+// });
+
+// server.listen(config.port, function () {
+//     console.log('Server is running.. port '+ config.port); 
+// });
 
 socketsController.socketsOn(io);
 

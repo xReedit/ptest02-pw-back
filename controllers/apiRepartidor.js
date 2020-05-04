996b51f44df4487593b2131c9f7c357bab28bccb
marcelo <xreedit@gmail.com>
2020-05-03 21:00:24 -0500

    pruebas 1.3

--- controllers/apiRepartidor.js
+++ controllers/apiRepartidor.js
@@ -23,18 +23,18 @@ const setRepartidorConectado = function (dataCLiente) {
 module.exports.setRepartidorConectado = setRepartidorConectado;
 
 const setEfectivoMano = function (req, res) {  
-	console.log('llego a funcion setEfectivoMano');
-	console.log('llego a funcion setEfectivoMano req', req);
-	console.log('llego a funcion setEfectivoMano req usuariotoken', req.usuariotoken);
+	// console.log('llego a funcion setEfectivoMano');
+	// console.log('llego a funcion setEfectivoMano req', req);
+	// console.log('llego a funcion setEfectivoMano req usuariotoken', req.usuariotoken);
 
-	//const idrepartidor = managerFilter.getInfoToken(req,'idrepartidor');
-	// const efectivo = req.body.efectivo;      
-	// const online = req.body.online;     
+	const idrepartidor = 1; // managerFilter.getInfoToken(req,'idrepartidor');
+	const efectivo = req.body.efectivo;      
+	const online = req.body.online;     
 
-	//console.log('llego a funcion setEfectivoMano idrepartidor', idrepartidor);
+	console.log('llego a funcion setEfectivoMano idrepartidor', idrepartidor);
 	
- //    const read_query = `update repartidor set efectivo_mano = ${efectivo}, online = ${online} where idrepartidor = ${idrepartidor}`;
- //    emitirRespuesta_RES(read_query, res);        
+    const read_query = `update repartidor set efectivo_mano = ${efectivo}, online = ${online} where idrepartidor = ${idrepartidor}`;
+    emitirRespuesta_RES(read_query, res);        
 }
 module.exports.setEfectivoMano = setEfectivoMano;
 

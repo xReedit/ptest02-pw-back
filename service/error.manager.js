72a22bcdf17792f8c18c82a7a47a1826d944deb1
Marcelo <macraze.info@gmail.com>
2024-07-15 18:37:13 -0500

    error

--- service/error.manager.js
+++ service/error.manager.js
@@ -1,5 +1,5 @@
 let config = require('../_config');
-let {Sequelize, QueryTypes} = require('sequelize');
+let Sequelize = require('sequelize');
 let sequelize = new Sequelize(config.database, config.username, config.password, config.sequelizeOption);
 
 
@@ -10,12 +10,12 @@ const logError = function (payload) {
         const errorString = JSON.stringify(data);
         const query = `INSERT INTO historial_error (error, origen, fecha) VALUES ('${errorString}', '${data.origen}', NOW())`;
         console.log(query);
-    
-        sequelize.query(query, { type: QueryTypes.INSERT })
-            .then(function (rows) {
-                console.log('Error logged successfully');
-            })
-            .catch((err) => { console.error('Error logging error:', err); });
+            
+        sequelize.query(query, { type: sequelize.QueryTypes.INSERT })
+            // .then(function (rows) {
+            //     console.log('Error logged successfully');
+            // })
+            // .catch((err) => { console.error('Error logging error:', err); });
     } catch (error) {
         console.error('Error logging error:', error);        
     }
